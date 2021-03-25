20c0b4f50e2172c884bc70c5710d7ba51dd52b80
ns-yamashita <daito.yamashita@na-s.jp>
2021-03-25 10:18:30 +0900

    Initial Commit

---
+++ Chapter19-5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter19-5
+//
+//  Created by daito yamashita on 2021/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
